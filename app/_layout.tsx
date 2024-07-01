d2e1e313144363b0f8c4235037078903d58e44cc
reewaz401 <39514982+reewaz401@users.noreply.github.com>
2024-07-01 11:11:14 +0200

    splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="splash" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,10 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import LoginScreen from '@/screens/LoginScreen';
+import SplashScreen from './splash';
 
 export default function HomeScreen() {
   return (
-   <LoginScreen/>
+   <SplashScreen/>
   );
 }
 
