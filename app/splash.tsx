a07bc61e4a2235d78d7adb7ac98061ebe85eca6c
reewaz401 <39514982+reewaz401@users.noreply.github.com>
2024-07-08 12:25:13 +0200

    splashscreen

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,10 +1,11 @@
 import { Colors } from '@/constants/Colors';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Image } from 'react-native';
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import Button from '@/components/Button';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@/app/index';
@@ -21,11 +22,17 @@ export default function SplashScreen() {
     
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Welcome to the Splash Screen!</Text>
-           
-            <Text style={styles.buttonStart} onPress={handleStart}>Start</Text>
-       
+        <View style={styles.centeredContent}>
+          <Image source={require('@/assets/images/logo.png')} style={styles.logo} />
+          <Text style={styles.text}>Tech no. 8</Text>
         </View>
+        <View style={styles.buttonContainer}>
+          <Button 
+            label="Start" 
+            onClick={handleStart}
+          />
+        </View>
+      </View>
     );
 };
 
@@ -34,22 +41,25 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    text: {
+      },
+      centeredContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      logo: {
+        // width: 100,
+        // height: 100,
+        marginBottom: 20,
+      },
+      text: {
         fontSize: 20,
-        fontWeight: 'bold',
-        color: Colors.light.text,
-    },
-    buttonStart: {
-        fontSize: hp(3),
-        backgroundColor: Colors.light.background,
         textAlign: 'center',
-        textAlignVertical: 'center',
-        height: hp(7),
-        width: wp(80),
-        borderRadius: 8,
-        margin: hp(3),
-        color: 'white',
-        
-    },
+        marginBottom: 20,
+      },
+      buttonContainer: {
+        position: 'absolute',
+        bottom: 30,
+        width: '100%',
+        paddingHorizontal: 20,
+      },
 });
