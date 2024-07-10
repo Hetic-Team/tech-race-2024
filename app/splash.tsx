b4b80f865ebf03990052042fe431930e1bf841fb
Ces4rRsLine <167258734+achkeydev@users.noreply.github.com>
2024-07-10 13:59:02 +0200

    responsive splash screen
    modfying some stupid style to get the most responsive scheme

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,6 +1,6 @@
 import { Colors } from '@/constants/Colors';
 import React from 'react';
-import { View, Text, StyleSheet,Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
@@ -10,29 +10,27 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@/app/index';
 
-
 export default function SplashScreen() {
-
     const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
     const handleStart = () => {
-      console.log("Button Start")
-      navigation.navigate('Login');
+        console.log("Button Start")
+        navigation.navigate('Login');
     };
-    
+
     return (
         <View style={styles.container}>
-        <View style={styles.centeredContent}>
-          <Image source={require('@/assets/images/logo.png')} style={styles.logo} />
-          <Text style={styles.text}>Tech no. 8</Text>
+            <View style={styles.centeredContent}>
+                <Image source={require('@/assets/images/logo.png')} style={styles.logo} />
+                <Text style={styles.text}>Tech no. 8</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button 
+                    label="Start" 
+                    onClick={handleStart}
+                />
+            </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <Button 
-            label="Start" 
-            onClick={handleStart}
-          />
-        </View>
-      </View>
     );
 };
 
@@ -41,25 +39,29 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-      },
-      centeredContent: {
+        backgroundColor: Colors.background, // couleur background
+    },
+    centeredContent: {
         justifyContent: 'center',
         alignItems: 'center',
-      },
-      logo: {
-        // width: 100,
-        // height: 100,
-        marginBottom: 20,
-      },
-      text: {
-        fontSize: 20,
+        marginBottom: hp('10%'),
+    },
+    logo: {
+        width: wp('50%'), // 50% width
+        height: hp('25%'), // 25% height
+        marginBottom: hp('5%'), // margin bottom
+        resizeMode: 'contain', // meme aspect ratio
+    },
+    text: {
+        fontSize: wp('5%'), // font size
         textAlign: 'center',
-        marginBottom: 20,
-      },
-      buttonContainer: {
+        color: Colors.textPrimary, // ytext color
+        marginBottom: hp('5%'), // bottom margin
+    },
+    buttonContainer: {
         position: 'absolute',
-        bottom: 30,
+        bottom: hp('5%'), // bottom
         width: '100%',
-        paddingHorizontal: 20,
-      },
+        paddingHorizontal: wp('10%'), // padding horizontal
+    },
 });
